c7dd1b49ae823d5bdf1443d15b8a47b860433c3a
SerchAlex13 <sergio.rosales5262@alumnos.udg.mx>
2022-10-19 16:00:20 -0500

    Inicio CSS

--- resources/views/prendas/prendaCreate.blade.php
+++ resources/views/prendas/prendaCreate.blade.php
@@ -9,42 +9,100 @@
     @vite(['resources/css/bootstrap.css', 'resources/js/bootstrapjs.js'])
 </head>
 <body>
-    <h1>Crear Torneo</h1>
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">DSA Diseños</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
         </div>
-    @endif
+    </nav>
 
-    <form action="/prenda" method="POST">
-        @csrf
-        <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre"">
-        <br>
-        <label for="tipo">Tipo</label>
-        <input type="text" name="tipo" id="tipo">
-        <br>
-        <label for="descripcion">Descripción</label>
-        <input type="text" name="descripcion" id="descripcion">
-        <br>
-        <label for="color">Color</label>
-        <input type="text" name="color" id="color">
-        <br>
-        <label for="talla">Talla</label>
-        <input type="text" name="talla" id="talla">
-        <br>
-        <label for="tela">Tela</label>
-        <input type="text" name="tela" id="tela">
-        <br>
-        <label for="inventario">Inventario</label>
-        <input type="number" name="inventario" id="inventario">
-        <br>
-        <input type="submit" value="Guardar">
-    </form>
+    <div class="container">
+        <h1>Crear Prenda</h1>
+    </div>
+    
+    <div class="container">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-8">
+                <form action="/prenda" method="POST" class="row g-3">
+                    @csrf
+                    <div class="col-md-8">
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input type="text" class="form-control" name="nombre" id="nombre">
+                    </div>
+                    <div class="col-md-8">
+                        <label for="tipo" class="form-label">Tipo</label>
+                        <input type="text" class="form-control" name="tipo" id="tipo">
+                    </div>
+                    <div class="col-8">
+                        <label for="descripcion" class="form-label">Descripción</label>
+                        <input type="text" class="form-control" name="descripcion" id="descripcion">
+                    </div>
+                    <div class="col-8">
+                        <label for="color" class="form-label">Color</label>
+                        <input type="text" class="form-control" name="color" id="color">
+                    </div>
+                    <div class="col-md-8">
+                        <label for="talla" class="form-label">Talla</label>
+                        <input type="text" class="form-control" name="talla" id="talla">
+                    </div>
+                    <div class="col-md-8">
+                        <label for="tela" class="form-label">Tela</label>
+                        <input type="text" class="form-control" name="tela" id="tela">
+                    </div>
+                    <div class="col-md-8">
+                        <label for="inventario" class="form-label">Inventario</label>
+                        <input type="text" class="form-control" name="inventario" id="inventario">
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                    </div>
+                </form>
+            </div>
+        </div>    
+    </div>
+
 </body>
 </html>
--- resources/views/prendas/prendaIndex.blade.php
+++ resources/views/prendas/prendaIndex.blade.php
@@ -10,106 +10,96 @@
 </head>
 <body>
 
-    
-  <header>
-    <div class="px-3 py-2 text-bg-dark">
-      <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-          </a>
 
-          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
-            <li>
-              <a href="#" class="nav-link text-secondary">
-                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"/></svg>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" class="nav-link text-white">
-                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#speedometer2"/></svg>
-                Dashboard
-              </a>
-            </li>
-            <li>
-              <a href="#" class="nav-link text-white">
-                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#table"/></svg>
-                Orders
-              </a>
-            </li>
-            <li>
-              <a href="#" class="nav-link text-white">
-                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#grid"/></svg>
-                Products
-              </a>
-            </li>
-            <li>
-              <a href="#" class="nav-link text-white">
-                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
-                Customers
-              </a>
-            </li>
-          </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">DSA Diseños</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
         </div>
-      </div>
+    </nav>
+
+    <div class="container">
+        <h1>Listado de Prendas</h1>
     </div>
-    <div class="px-3 py-2 border-bottom mb-3">
-      <div class="container d-flex flex-wrap justify-content-center">
-        <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
-          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-        </form>
 
-        <div class="text-end">
-          <button type="button" class="btn btn-light text-dark me-2">Login</button>
-          <button type="button" class="btn btn-primary">Sign-up</button>
-        </div>
-      </div>
+    <div class="container">
+        <a href="/prenda/create" class="btn btn-primary">Crear Nueva Prenda</a>
     </div>
-  </header>
 
-    <h1>Listado de Prendas</h1>
-
-    <a href="/prenda/create">Crear Nueva Prenda</a>
-
-    <table border="1">
-        <tr>
-            <th>Nombre</th>
-            <th>Tipo</th>
-            <th>Descripción</th>
-            <th>Color</th>
-            <th>Talla</th>
-            <th>Tela</th>
-            <th>Innventario</th>
-            <th>Editar</th>
-            <th>Eliminar</th>
-        </tr>
-        @foreach ($prendas as $prenda)
-            <tr>
-                <td>
-                    <a href="/prenda/{{ $prenda->id }}">
-                        {{ $prenda->nombre }}
-                    </a>
-                </td>
-                <td>{{ $prenda->tipo }}</td> 
-                <td>{{ $prenda->descripcion }}</td> 
-                <td>{{ $prenda->color }}</td> 
-                <td>{{ $prenda->talla }}</td> 
-                <td>{{ $prenda->tela }}</td>
-                <td>{{ $prenda->inventario }}</td> 
-                <td>
-                    <a href="/prenda/{{ $prenda->id }}/edit">Editar</a>
-                </td>
-                <td>
-                    <form action="/prenda/{{ $prenda->id }}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" value="Borrar">
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </table>
+    <div class="container">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Tipo</th>
+                    <th>Descripción</th>
+                    <th>Color</th>
+                    <th>Talla</th>
+                    <th>Tela</th>
+                    <th>Inventario</th>
+                    <th>Editar</th>
+                    <th>Eliminar</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                @foreach ($prendas as $prenda)
+                    <tr>
+                        <td>
+                            <a class="nav-link" href="/prenda/{{ $prenda->id }}">
+                                {{ $prenda->nombre }}
+                            </a>
+                        </td>
+                        <td>{{ $prenda->tipo }}</td> 
+                        <td>{{ $prenda->descripcion }}</td> 
+                        <td>{{ $prenda->color }}</td> 
+                        <td>{{ $prenda->talla }}</td> 
+                        <td>{{ $prenda->tela }}</td>
+                        <td>{{ $prenda->inventario }}</td> 
+                        <td>
+                            <a href="/prenda/{{ $prenda->id }}/edit" class="btn btn-outline-dark">Editar</a>
+                        </td>
+                        <td>
+                            <form action="/prenda/{{ $prenda->id }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="Borrar" class="btn btn-outline-dark">
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
     
 
